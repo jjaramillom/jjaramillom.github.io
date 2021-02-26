5d8c75b7992efef348419fd71d912e986a47e879
Jacobo Jaramillo-Molina <Jacobo.Jaramilo-Molina@kuka.com>
2021-02-26 21:28:13 +0100

    remove floating effect icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {graphql, useStaticQuery, PageProps} from 'gatsby';
 import Container from 'react-bootstrap/Container';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faLinkedin, faGithubSquare, IconDefinition} from '@fortawesome/free-brands-svg-icons';
 import {faEnvelopeSquare, faIdCard} from '@fortawesome/free-solid-svg-icons';
-import cloneDeep from 'lodash/cloneDeep';
 
 import Layout from 'components/Layout/Layout';
 import Typewriter from 'components/Typewriter/Typewriter';
@@ -15,42 +13,25 @@ type IconConfig = {
 	icon: IconDefinition;
 	link: string;
 	label: string;
-	animationActive: boolean;
 };
 
-const DEFAULT_ICONS: IconConfig[] = [
+const ICONS: IconConfig[] = [
 	{
 		icon: faGithubSquare,
 		label: 'Github',
 		link: 'https://www.github.com/jjaramillom',
-		animationActive: false,
 	},
 	{
 		icon: faLinkedin,
 		label: 'LinkedIn',
 		link: 'https://www.linkedin.com/in/jjaramillom',
-		animationActive: false,
 	},
-	{icon: faEnvelopeSquare, label: 'Send me an email', link: 'mailto:jjaramillom@unal.edu.co', animationActive: false},
-	{icon: faIdCard, label: 'Download my CV', link: '/static/resume.pdf', animationActive: false}, // ../../resume.pdf
+	{icon: faEnvelopeSquare, label: 'Send me an email', link: 'mailto:jjaramillom@unal.edu.co'},
+	{icon: faIdCard, label: 'Download my CV', link: '/static/resume.pdf'}, // ../../resume.pdf
 ];
 
 const IndexPage = ({uri}: PageProps) => {
 	const siteMetadata = useStaticQuery<GatsbyTypes.Query>(query).site?.siteMetadata;
-
-	const [icons, setIcons] = useState<IconConfig[]>(DEFAULT_ICONS);
-
-	useEffect(() => {
-		icons.forEach((_, i) => {
-			setTimeout(() => {
-				setIcons(icons => {
-					const iconsCopy = cloneDeep(icons);
-					iconsCopy[i].animationActive = true;
-					return iconsCopy;
-				});
-			}, i * 200);
-		});
-	}, []);
 
 	return (
 		<Layout uri={uri}>
@@ -61,18 +42,12 @@ const IndexPage = ({uri}: PageProps) => {
 				<Typewriter />
 				<hr className="my-3 w-100" />
 				<div className="d-flex flex-row justify-content-center mt-5">
-					{icons.map(({icon, link, animationActive, label}) => (
+					{ICONS.map(({icon, link, label}) => (
 						<div className={`${classes.icon_wrapper}`}>
-							<a
-								href={link}
-								className={animationActive ? classes.animated : ''}
-								target="_blank"
-								rel="noopener noreferrer"
-								download={link === '/static/resume.pdf'}
-							>
+							<a href={link} target="_blank" rel="noopener noreferrer" download={link === '/static/resume.pdf'}>
 								<FontAwesomeIcon aria-hidden className={`${classes.icon} `} icon={icon} />
-								<div className={`mt-3 ${classes.tooltip}`} id={link}>{`${label}`}</div>
 							</a>
+							<div className={`mt-1 ${classes.tooltip}`}>{`${label}`}</div>
 						</div>
 					))}
 				</div>

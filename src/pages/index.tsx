14666bdd71c0246db77baa6f7578088d97ba1a24
Jacobo Jaramillo-Molina <Jacobo.Jaramilo-Molina@kuka.com>
2021-02-27 16:00:33 +0100

    add some tech icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,20 @@ import React from 'react';
 import {graphql, useStaticQuery, PageProps} from 'gatsby';
 import Container from 'react-bootstrap/Container';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faLinkedin, faGithubSquare, IconDefinition} from '@fortawesome/free-brands-svg-icons';
+import {
+	IconDefinition,
+	faLinkedin,
+	faGithubSquare,
+	faJs,
+	faCss3,
+	faHtml5,
+	faReact,
+	faVuejs,
+	faNodeJs,
+	faPython,
+	faDocker,
+	faGit,
+} from '@fortawesome/free-brands-svg-icons';
 import {faEnvelopeSquare, faIdCard} from '@fortawesome/free-solid-svg-icons';
 
 import Layout from 'components/Layout/Layout';
@@ -10,13 +23,30 @@ import Typewriter from 'components/Typewriter/Typewriter';
 import classes from './index.module.scss';
 import resume from '../../static/resume.pdf';
 
-type IconConfig = {
+type ContactIconConfig = {
 	icon: IconDefinition;
 	link: string;
 	label: string;
 };
 
-const ICONS: IconConfig[] = [
+type TechIconConfig = {
+	icon: IconDefinition;
+	label: string;
+};
+
+const TECH_ICONS: TechIconConfig[] = [
+	{icon: faJs, label: 'JavaScript'},
+	{icon: faReact, label: 'React.js'},
+	{icon: faVuejs, label: 'Vue.js'},
+	{icon: faCss3, label: 'CSS'},
+	{icon: faHtml5, label: 'HTML'},
+	{icon: faNodeJs, label: 'Node.js'},
+	{icon: faPython, label: 'Python'},
+	{icon: faDocker, label: 'Docker'},
+	{icon: faGit, label: 'Git'},
+];
+
+const CONTACT_ICONS: ContactIconConfig[] = [
 	{
 		icon: faGithubSquare,
 		label: 'Github',
@@ -37,17 +67,26 @@ const IndexPage = ({uri}: PageProps) => {
 	return (
 		<Layout uri={uri}>
 			<Container className="mt-3">
-				<h1 className={classes.name_title}>
+				<h1 className={`${classes.title} ${classes.name}`}>
 					<span>{siteMetadata?.author?.name}</span> <span>{siteMetadata?.author?.lastName}</span>
 				</h1>
 				<Typewriter />
 				<hr className="my-3 w-100" />
-				<div className="d-flex flex-row justify-content-center mt-5">
-					{ICONS.map(({icon, link, label}) => (
-						<div key={link} className={`${classes.icon_wrapper}`}>
+				<div className="d-flex flex-row justify-content-center my-5">
+					{CONTACT_ICONS.map(({icon, link, label}) => (
+						<div key={link} className={classes.icon_wrapper}>
 							<a href={link} target="_blank" rel="noopener noreferrer" download={label === 'Download my CV'}>
-								<FontAwesomeIcon aria-hidden className={`${classes.icon} `} icon={icon} />
+								<FontAwesomeIcon aria-hidden className={`${classes.icon} ${classes.animated}`} icon={icon} />
 							</a>
+							<div className={`mt-1 ${classes.tooltip}`}>{`${label}`}</div>
+						</div>
+					))}
+				</div>
+				<h3 className={classes.tech_title}>These are some of the technologies I have worked with</h3>
+				<div className={classes.tech_grid}>
+					{TECH_ICONS.map(({icon, label}) => (
+						<div key={label} className={`${classes.icon_wrapper} ${classes.tech}`}>
+							<FontAwesomeIcon aria-hidden className={`${classes.icon} ${classes.tech} ${classes.animated}`} icon={icon} />
 							<div className={`mt-1 ${classes.tooltip}`}>{`${label}`}</div>
 						</div>
 					))}

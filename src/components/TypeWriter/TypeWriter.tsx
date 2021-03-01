dba15446ec8f740d190d7358226325684664bf23
Jacobo Jaramillo-Molina <Jacobo.Jaramilo-Molina@kuka.com>
2021-03-01 22:20:35 +0100

    add typewriter delay

--- src/components/TypeWriter/TypeWriter.tsx
+++ src/components/TypeWriter/TypeWriter.tsx
@@ -2,30 +2,38 @@ import React, {useContext, useState, useEffect} from 'react';
 
 import classes from './TypeWriter.module.scss';
 
+const START_DELAY = 500;
+
 const TypeWriter = () => {
-	const [delayedTypewritterClass, setDelayedTypewritterClass] = useState('');
+	const [firstDelayedTypewritterClass, setFirstDelayedTypewritterClass] = useState('');
+	const [secondDelayedTypewritterClass, setSecondDelayedTypewritterClass] = useState('');
 
 	useEffect(() => {
-		const classTimeout = setTimeout(() => setDelayedTypewritterClass(classes.active), 4000);
+		const firstTimeout = setTimeout(() => setFirstDelayedTypewritterClass(classes.active), START_DELAY);
+		const secondTimeout = setTimeout(() => {
+			setFirstDelayedTypewritterClass('');
+			setSecondDelayedTypewritterClass(classes.active);
+		}, 4000 + START_DELAY);
 		return () => {
-			clearTimeout(classTimeout);
+			clearTimeout(firstTimeout);
+			clearTimeout(secondTimeout);
 		};
 	}, []);
 
 	return (
 		<div className="text-center">
 			<div
-				className={`d-md-inline-block ${classes.typewriter} ${
-					delayedTypewritterClass === '' ? `${classes.active}` : ''
-				}`}
+				className={`d-md-inline-block ${classes.typewriter} ${firstDelayedTypewritterClass} 
+				${firstDelayedTypewritterClass !== '' ? `${classes.active}` : ''}`}
+				style={{opacity: firstDelayedTypewritterClass === '' && secondDelayedTypewritterClass === '' ? 0 : 1}}
 			>
 				<p>
 					Electronic Engineer by title,<span className="d-none d-sm-none d-md-inline-block">&nbsp;</span>
 				</p>
 			</div>
 			<div
-				className={`d-md-inline-block ${classes.typewriter} ${delayedTypewritterClass}`}
-				style={{opacity: delayedTypewritterClass === '' ? 0 : 1}}
+				className={`d-md-inline-block ${classes.typewriter} ${secondDelayedTypewritterClass}`}
+				style={{opacity: secondDelayedTypewritterClass === '' ? 0 : 1}}
 			>
 				<p>Software Engineer by heart</p>
 			</div>
